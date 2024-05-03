553d8e57f39f8796c1619dd208e1ca7f27e665f9
ratikaewkam <ratikaewkam.rk@gmail.com>
2024-05-03 09:51:19 +0700

    Update Master (state)

--- I2C/Master/Master.ino
+++ I2C/Master/Master.ino
@@ -1,9 +1,28 @@
+#define is_ready_btn 2
+#define is_active 4
+
+int is_ready_state = 1;
+bool is_ready = true;
+
 void setup() {
   Serial.begin(9600);
+  pinMode(is_ready_btn, INPUT);
+  pinMode(is_active, OUTPUT);
 }
 
 void loop() {
+  is_ready_state = digitalRead(is_ready_btn);
   int poten = analogRead(A0);
-  Serial.println(poten);
-  delay(1500);
+
+  if (is_ready){
+    digitalWrite(is_active, HIGH);
+  } else {
+    digitalWrite(is_active, LOW);
+  }
+  
+  if (is_ready_state == LOW) {
+    is_ready = !is_ready;
+  }
+  
+  delay(250);
 }
