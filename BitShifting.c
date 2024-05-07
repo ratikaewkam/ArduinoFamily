d5d6b3dde40feb266b98b2aee397934abbf4437f
ratikaewkam <ratikaewkam.rk@gmail.com>
2024-05-07 18:48:03 +0700

    Update BitShifting.c

--- BitShifting.c
+++ BitShifting.c
@@ -1,6 +1,47 @@
 #include <stdio.h>
 
 int main() {
-	printf("Hello, World!");
+	int num = 62599; // 1111010010000111
+	int data[2]; // 2 bytes
+	int i, result;
+	
+	printf("num: %d\n", num);
+	
+	/*
+		0xFF -> 255 -> 11111111
+	*/
+	
+	data[0] = num >> 8; // Left Shift
+	/*
+		1111010010000111 (62599)
+		0000000011110100 (244)
+	*/
+	
+	data[1] = num & 0xFF;
+	/*
+		1111010010000111 (62599)
+				&
+		0000000011111111 (255)	
+		0000000010000111 (135)
+	*/
+	
+	printf("data[0]: %d, data[1]: %d\n", data[0], data[1]);
+	
+	result = data[0];
+	/*
+		0000000011110100 (244)
+	*/
+	
+	result = (result << 8) | data[1]; // Right Shift
+	/*
+		0000000011110100 (244)
+		<< 8
+		1111010000000000 (62464)
+				| 
+		0000000010000111 (135)
+		1111010010000111 (62599)
+	*/
+	printf("result: %d", result);
+	
 	return 0;
 }
